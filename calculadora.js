6ea937a2653edc2d85a2b09d6031152f515aedb1
sebastian <sebastian.lorca@wic.uchile.cl>
2020-08-19 22:06:08 -0400

    es6

--- calculadora.js
+++ calculadora.js
@@ -1,99 +1,98 @@
+init= ()=>{
 
-function init(){
+    const resultado = document.getElementById('resultado');
+    const reset = document.getElementById('reset');
+    const suma = document.getElementById('suma');
+    const resta = document.getElementById('resta');
+    const multiplicacion = document.getElementById('multiplicacion');
+    const division = document.getElementById('division');
+    const igual = document.getElementById('igual');
+    const uno = document.getElementById('uno');
+    const dos = document.getElementById('dos');
+    const tres = document.getElementById('tres');
+    const cuatro = document.getElementById('cuatro');
+    const cinco = document.getElementById('cinco');
+    const seis = document.getElementById('seis');
+    const siete = document.getElementById('siete');
+    const ocho = document.getElementById('ocho');
+    const nueve = document.getElementById('nueve');
+    const cero = document.getElementById('cero');
 
-    var resultado = document.getElementById('resultado');
-    var reset = document.getElementById('reset');
-    var suma = document.getElementById('suma');
-    var resta = document.getElementById('resta');
-    var multiplicacion = document.getElementById('multiplicacion');
-    var division = document.getElementById('division');
-    var igual = document.getElementById('igual');
-    var uno = document.getElementById('uno');
-    var dos = document.getElementById('dos');
-    var tres = document.getElementById('tres');
-    var cuatro = document.getElementById('cuatro');
-    var cinco = document.getElementById('cinco');
-    var seis = document.getElementById('seis');
-    var siete = document.getElementById('siete');
-    var ocho = document.getElementById('ocho');
-    var nueve = document.getElementById('nueve');
-    var cero = document.getElementById('cero');
-
-    uno.onclick = function(e){
-        resultado.textContent = resultado.textContent + '1';
+    uno.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '1';
     }
-    dos.onclick = function(e){
-        resultado.textContent = resultado.textContent + '2';
+    dos.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '2';
     }
-    tres.onclick = function(e){
-        resultado.textContent = resultado.textContent + '3';
+    tres.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '3';
     }
-    cuatro.onclick = function(e){
-        resultado.textContent = resultado.textContent + '4';
+    cuatro.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '4';
     }
-    cinco.onclick = function(e){
-        resultado.textContent = resultado.textContent + '5';
+    cinco.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '5';
     }
-    seis.onclick = function(e){
-        resultado.textContent = resultado.textContent + '6';
+    seis.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '6';
     }
-    siete.onclick = function(e){
-        resultado.textContent = resultado.textContent + '7';
+    siete.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '7';
     }
-    ocho.onclick = function(e){
-        resultado.textContent = resultado.textContent + '8';
+    ocho.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '8';
     }
-    nueve.onclick = function(e){
-        resultado.textContent = resultado.textContent + '9';
+    nueve.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '9';
     }
-    cero.onclick = function(e){
-        resultado.textContent = resultado.textContent + '0';
+    cero.onclick = (e) =>{
+        resultado.innerHTML = resultado.innerHTML + '0';
     }
 
-    reset.onclick = function(e){
+    reset.onclick = (e) =>{
         resetear();
     }
 
-    suma.onclick = function(e){
-        operandoa = resultado.textContent;
+    suma.onclick = (e) =>{
+        operandoa = resultado.innerHTML;
         operacion = '+';
         limpiar();
     }
-    resta.onclick = function(e){
-        operandoa = resultado.textContent;
+    resta.onclick = (e) =>{
+        operandoa = resultado.innerHTML;
         operacion = '-';
         limpiar();
     }
-    multiplicacion.onclick = function(e){
-        operandoa = resultado.textContent;
+    multiplicacion.onclick = (e) =>{
+        operandoa = resultado.innerHTML;
         operacion = '*';
         limpiar();
     }
-    division.onclick = function(e){
-        operandoa = resultado.textContent;
+    division.onclick = (e) =>{
+        operandoa = resultado.innerHTML;
         operacion = '/';
         limpiar();
     }
 
-    igual.onclick = function(e){
-        operandob = resultado.textContent;
+    igual.onclick = (e) =>{
+        operandob = resultado.innerHTML;
         resolver();
     }
 }
 
-function limpiar(){
-    resultado.textContent = '';
+limpiar= ()=>{
+    resultado.innerHTML = '';
 }
 
-function resetear(){
-    resultado.textContent = '';
+resetear= ()=>{
+    resultado.innerHTML = '';
     operandoa = 0;
     operandob = 0;
     operacion = '';
 }
 
-function resolver(){
-    var res = 0;
+resolver= ()=>{
+    let res = 0;
     switch(operacion){
         case '+':
             res = parseFloat(operandoa) + parseFloat(operandob);
@@ -109,5 +108,5 @@ function resolver(){
             break;
     }
     resetear();
-    resultado.textContent = res;
+    resultado.innerHTML = res;
 }
